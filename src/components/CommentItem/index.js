20262dcd592e4c127df70df469455509e36b317a
Fikri Yusrihan <47127883+fikriyusrihan@users.noreply.github.com>
2023-08-12 13:07:28 +0700

    show comment's timestamp

--- src/components/CommentItem/index.js
+++ src/components/CommentItem/index.js
@@ -1,24 +1,26 @@
-import { Text, CardBody, Card, StackDivider, Stack, Box } from '@chakra-ui/react';
+import { Text, CardBody, Card, Stack } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 
 Index.propTypes = {
   comment: PropTypes.shape({
     username: PropTypes.string.isRequired,
-    comment: PropTypes.string.isRequired
+    comment: PropTypes.string.isRequired,
+    timestamp: PropTypes.string.isRequired
   })
 };
 
 export default function Index({ comment }) {
+  const timestamp = new Date(comment.timestamp);
+
   return (
     <Card flexShrink="0">
       <CardBody>
-        <Stack divider={<StackDivider />} spacing="4">
-          <Box>
-            <Text fontSize="sm">
-              <span style={{ fontWeight: 'bold', marginRight: '4px' }}>{comment.username}</span>
-              {comment.comment}
-            </Text>
-          </Box>
+        <Stack spacing="4" gap={0}>
+          <Text fontSize="xs">{timestamp.toLocaleString()}</Text>
+          <Text fontSize="sm">
+            <span style={{ fontWeight: 'bold', marginRight: '4px' }}>{comment.username}</span>
+            {comment.comment}
+          </Text>
         </Stack>
       </CardBody>
     </Card>
